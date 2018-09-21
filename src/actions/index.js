6a19652f16014b52d0dc023d97ce2ba5f6a1f745
Hoon Lee <yl.hoony@gmail.com>
2018-09-21 12:37:38 -0700

    set createPost, fetchPost, deletePost

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,48 @@
 import axios from "axios";
 export const FETCH_POSTS = "fetch_posts";
+export const CREATE_POST = "create_post";
+export const FETCH_POST = "fetch_post";
+export const DELETE_POST = "delete_post";
 
 const ROOT_URL = "http://reduxblog.herokuapp.com/api";
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const fetchPosts = () => {
-  const url = `${ROOT_URL}/posts${process.env.REACT_APP_API_KEY}`;
+  const url = `${ROOT_URL}/posts${API_KEY}`;
   const req = axios.get(url);
 
   return {
     type: FETCH_POSTS,
     payload: req
+  };
+};
+
+export const createPost = (values, callback) => {
+  const url = `${ROOT_URL}/posts${API_KEY}`;
+  const req = axios.post(url, values).then(() => callback());
+
+  return {
+    type: CREATE_POST,
+    payload: req
+  };
+};
+
+export const fetchPost = id => {
+  const url = `${ROOT_URL}/posts/${id}${API_KEY}`;
+  const req = axios.get(url);
+
+  return {
+    type: FETCH_POST,
+    payload: req
+  };
+};
+
+export const deletePost = (id, callback) => {
+  const url = `${ROOT_URL}/posts/${id}${API_KEY}`;
+  const req = axios.delete(url).then(() => callback());
+
+  return {
+    type: DELETE_POST,
+    payload: id
   };
 };
