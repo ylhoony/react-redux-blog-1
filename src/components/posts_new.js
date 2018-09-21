75dea7099ed3100ddb39f9b29d3e60cc99625c4a
Hoon Lee <yl.hoony@gmail.com>
2018-09-20 17:13:23 -0700

    created file and added contents

---
+++ src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div>post new</div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default PostsNew;
