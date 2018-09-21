e3e9f310fa122f7f1468473c2406c5eb48d397c9
Hoon Lee <yl.hoony@gmail.com>
2018-09-20 17:13:38 -0700

    set up routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import promise from "redux-promise";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from "./reducers";
+import PostIndex from "./components/posts_index";
+import PostsNew from "./components/posts_new";
+
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div className="container">
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostIndex} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
